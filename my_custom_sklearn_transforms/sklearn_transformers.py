7e258ebdf966dd31e2e192b9059267e57b89bae8
redsonx <36752311+redsonx@users.noreply.github.com>
2020-12-05 14:19:34 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,7 +14,4 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         
-        cleanup_nums = {"koi_pdisposition": {"CANDIDATE": 1, "FALSE POSITIVE": 2}}
-        data2 = data.replace(cleanup_nums, inplace=False)
-        
-        return data2.drop(labels=self.columns, axis='columns')
+        return data.drop(labels=self.columns, axis='columns')
